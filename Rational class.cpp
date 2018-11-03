152b72253fe291ce365724871ece7bf21d921654
KaZiUpl <38405516+KaZiUpl@users.noreply.github.com>
2018-11-03 19:36:22 +0100

    poprawki
    dodano operator>>,
    zmodyfikowano operator<<,
    poprawki wyglądu

--- Rational class.cpp
+++ Rational class.cpp
@@ -1,5 +1,8 @@
 #include<iostream>
+#include<string>
 #include<cmath>
+#include<algorithm>
+
 
 using namespace std;
 
@@ -7,13 +10,15 @@ class Rational
 {
 private:
     long int numerator, denominator;
+    static int GCD(long int a, long int b);
     void reduce();
 public:
-    static int GCD(long int a, long int b);
     Rational();
-    Rational(long int numerator,long int denominator);
+    Rational(long int numerator,long int denominator = 1);
     long int    get_numerator(),
                 get_denominator();
+    void        set_numerator(long int numerator),
+                set_denominator(long int denominator);
     Rational operator+(Rational other_one);
     Rational operator-(Rational other_one);
     Rational operator*(Rational other_one);
@@ -24,19 +29,21 @@ public:
     bool operator>=(Rational other_one);
     bool operator==(Rational other_one);
     bool operator!=(Rational other_one);
-};
 
-void Rational::reduce()
-{
-    int gcd = GCD(numerator,denominator);
-    numerator = numerator / gcd;
-    denominator = denominator / gcd;
-}
+    friend ostream &operator<<(ostream &output,const Rational &fraction);
+    friend istream &operator>>(istream &input, Rational &fraction);
+};
 
 int Rational::GCD(long int a, long int b)
 {
     if(b == 0) return abs(a);
     else       return GCD(abs(b), abs(a % b)) ;
+}
+void Rational::reduce()
+{
+    int gcd = GCD(numerator,denominator);
+    numerator = numerator / gcd;
+    denominator = denominator / gcd;
 }
 
 Rational::Rational()
@@ -57,6 +64,15 @@ long int Rational::get_denominator()
 {
     return denominator;
 }
+void Rational::set_numerator(long int numerator)
+{
+    this->numerator = numerator;
+}
+void Rational::set_denominator(long int denominator)
+{
+    this->denominator = denominator;
+}
+
 Rational Rational::operator+(Rational other_one)
 {
     Rational result = Rational();
@@ -114,21 +130,38 @@ bool Rational::operator!=(Rational other_one)
     return ((this->get_numerator()*other_one.get_denominator()) != (this->get_denominator() * other_one.get_numerator()));
 }
 // ------------------------------ KONIEC RATIONAL -------------------------------
-ostream & operator<<(ostream &output,Rational fraction)
+ostream & operator<<(ostream &output,const Rational &fraction)
 {
-    if(fraction.get_numerator() == fraction.get_denominator()) cout<< 1;
-    else cout<<fraction.get_numerator() << "/" << fraction.get_denominator();
+    if(fraction.numerator == fraction.denominator) cout << 1;
+    else if(fraction.denominator == 1) cout<<fraction.numerator;
+    else cout<<fraction.numerator << "|" << fraction.denominator;
     return output;
 }
+istream & operator>>(istream &input, Rational &fraction)
+{
+    string input_string;
+    int position_of_line;
 
+    cin>>input_string;
+    position_of_line = input_string.find("|");
 
-
-
+    if(position_of_line == (int)string::npos)
+    {
+        fraction.numerator = strtol(input_string.c_str(),NULL,10);
+    }
+    else
+    {
+        string numerator,denominator;
+        numerator = input_string.substr(0,position_of_line);
+        denominator = input_string.substr(position_of_line+1,input_string.size()-position_of_line);
+        fraction.numerator = strtol(numerator.c_str(),NULL,10);
+        fraction.denominator = strtol(denominator.c_str(),NULL,10);
+    }
+    return input;
+}
 
 int main()
 {
-
-
 
     return 0;
 }
