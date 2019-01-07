df4f29cc4c29f1023a8b7c94162286848911b520
KaZiUpl <38405516+KaZiUpl@users.noreply.github.com>
2019-01-07 11:44:20 +0100

    szajs

---
+++ kolokwium/Beverages/Beverages.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+
+int main()
+{
+    int number_of_case = 1;
+    int number_of_beverages,
+        number_of_rules;
+
+        while()
+
+    return 0;
+}
---
+++ kolokwium/Identifying Tea/Identifying Tea.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+
+using namespace std;
+int main()
+{
+    int tea_type;
+    int answer;
+    int correct_answers = 0;
+
+    while(cin>>tea_type)
+    {
+        correct_answers = 0;
+        for(int i=0;i<5;i++)
+        {
+            cin>>answer;
+            if(answer == tea_type) correct_answers++;
+        }
+        cout<<correct_answers<<endl;
+    }
+
+    return 0;
+}
