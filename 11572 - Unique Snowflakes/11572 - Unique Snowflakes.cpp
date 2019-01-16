102c122ab4aca257ef43a9b30f089e8fd3478c9e
KaZiUpl <38405516+KaZiUpl@users.noreply.github.com>
2019-01-16 17:00:01 +0100

    dodano rozwiązanie

--- 11572 - Unique Snowflakes/11572 - Unique Snowflakes.cpp
+++ 11572 - Unique Snowflakes/11572 - Unique Snowflakes.cpp
@@ -2,46 +2,55 @@
 #include <string>
 #include <vector>
 #include <algorithm>
+#include <set>
 using namespace std;
-
-void try_to_add_snowflake_to_package(int snowflake_number,vector<int>  &package)
-{
-    if(package.size() > 0 && (snowflake_number < package[0] || snowflake_number > package[package.size()-1]))
-    {
-        package.push_back(snowflake_number);
-        return;
-    }
-    for(int i=0;i<package.size();i++)
-    {
-        if(package[i] == snowflake_number)
-        {
-            return;
-        }
-    }
-    package.push_back(snowflake_number);
-}
 
 int main()
 {
     int number_of_cases,
         number_of_snowflakes;
     int snowflake_number;
+    int biggest_package;
 
 
     cin>>number_of_cases;
     while(number_of_cases--)
     {
         cin>>number_of_snowflakes;
-        vector<int> snowflakes_package;
+        set<int> snowflakes_package;
+        vector<int> snowflakes;
+        biggest_package = 0;
+
         for(int i=0; i<number_of_snowflakes ; i++)
         {
             cin>>snowflake_number;
-
-            try_to_add_snowflake_to_package(snowflake_number,snowflakes_package);
-
-            sort(snowflakes_package.begin(),snowflakes_package.end());
+            snowflakes.push_back(snowflake_number);
         }
-        cout<<snowflakes_package.size()<<endl;
+
+        int start_index = 0,
+            end_index = 0;
+
+        while(end_index < number_of_snowflakes)
+        {
+            // adding as long as snowflakes are unique
+            while(end_index < number_of_snowflakes && !snowflakes_package.count(snowflakes[end_index]))
+            {
+                snowflakes_package.insert(snowflakes[end_index]);
+                end_index++;
+            }
+            // checking if package is bigger than the biggest found
+            if(end_index - start_index > biggest_package)
+            {
+                biggest_package = end_index - start_index;
+            }
+
+            // deleting snowflake that is not unique
+            snowflakes_package.erase(snowflakes[start_index]);
+            // moving start index forward and praying that now snowflake will be unique
+            start_index++;
+        }
+
+        cout<<biggest_package<<endl;
     }
 
 
